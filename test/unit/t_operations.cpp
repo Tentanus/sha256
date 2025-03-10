ad1b869c0c75f8d73669860b1b0ea736b0f58145
MaartyW@lap-pop <meweverling@gmail.com>
2025-03-10 23:45:29 +0100

    first tests

--- test/unit/t_operations.cpp
+++ test/unit/t_operations.cpp
@@ -4,3 +4,21 @@ extern "C"
 {
 #include "sha_operations.h"
 }
+
+TEST(ROTR, basic)
+{
+	EXPERCT_EQ(ROTR(0, 0), 0);
+	EXPERCT_EQ(ROTR(1, 0), 1);
+	EXPERCT_EQ(ROTR(0, 1), 0);
+	EXPERCT_EQ(ROTR(1, 1), 2);
+	// NOTE: add case for overflow from and two max
+}
+
+TEST(ROTL, basic)
+{
+	EXPERCT_EQ(ROTL(0, 0), 0);
+	EXPERCT_EQ(ROTL(1, 0), 1);
+	EXPERCT_EQ(ROTL(0, 1), 0);
+	EXPERCT_EQ(ROTL(2, 1), 1);
+	// NOTE: add case for overflow from and two max
+}
