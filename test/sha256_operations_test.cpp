c6a4986220810a9487cf6a784d99075d2b649a29
MaartyW@lap-pop <meweverling@gmail.com>
2025-05-05 15:20:26 +0200

    updated test to only use unsigned values, added comments for clarity

--- test/sha256_operations_test.cpp
+++ test/sha256_operations_test.cpp
@@ -1,22 +1,36 @@
-#include <gtest/gtest.h>
 #include "sha256.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
 
-TEST(sha256, sha256_operations_ROTL) {
-    // Simple Test cases
-    EXPECT_EQ(ROTL(0, 1), 0);
-    EXPECT_EQ(ROTL(1, 1), 2);
+TEST(sha256_operations, rotl)
+{
+	// Simple Test cases
+	EXPECT_EQ(ROTL(0x00000000, 1), 0x00000000);		// check if 0 stays 0
+	EXPECT_EQ(ROTL(0x00000001, 1), 0x00000002);		// standard check
+	EXPECT_EQ(ROTL(0x40000000, 1), 0x80000000);		// standard check higher value
 
-    EXPECT_EQ(ROTL(1073741824, 1), -2147483648);
-    EXPECT_EQ(ROTL(-2147483648, 1), 1);
-    EXPECT_EQ(ROTL(1431655765, 1), -1431655766);
+	EXPECT_EQ(ROTL(0x80000000, 1), 0x00000001);		// overflow the input
+	EXPECT_EQ(ROTL(0x55555555, 1), 0xAAAAAAAA);		// overflow the input
+
+	EXPECT_EQ(ROTL(0xFFFFFFFF, 1), 0xFFFFFFFF);		// see if -1 stays -1
+	EXPECT_EQ(ROTL(0xFFFFFFFF, 20), 0xFFFFFFFF);	// see if -1 stays -1
+
+	EXPECT_EQ(ROTL(0x00000001, 32), 0x00000001);	// overflow the rotation
 }
 
-TEST(sha256, sha256_operations_ROTR) {
-    // Simple Test cases
-    EXPECT_EQ(ROTR(0, 1), 0);
-    EXPECT_EQ(ROTR(2, 1), 1);
-    EXPECT_EQ(ROTR(1, 1), -2147483648);
-    EXPECT_EQ(ROTR(-2147483648, 1), 1073741824);
-    EXPECT_EQ(ROTL(1431655765, 1), -1431655766);
+TEST(sha256_operations, rotr)
+{
+	// Simple Test cases
+	EXPECT_EQ(ROTR(0x00000000, 1), 0x00000000);		// check if 0 stays 0
+	EXPECT_EQ(ROTR(0x00000002, 1), 0x00000001);		// standard check
+	EXPECT_EQ(ROTR(0x80000000, 1), 0x40000000);		// standard check higher value
+
+	EXPECT_EQ(ROTR(0x00000001, 1), 0x80000000);		// overflow the input
+	EXPECT_EQ(ROTR(0xAAAAAAAA, 1), 0x55555555);		// overflow the input
+
+	EXPECT_EQ(ROTR(0xFFFFFFFF, 1), 0xFFFFFFFF);		// see if -1 stays -1
+	EXPECT_EQ(ROTR(0xFFFFFFFF, 20), 0xFFFFFFFF);	// see if -1 stays -1
+
+	EXPECT_EQ(ROTR(0x00000001, 32), 0x00000001);	// overflow the rotation
 
 }
---
+++ test/sha256_options_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(sha256_options, dummy_test)
+{
+	EXPECT_EQ(1, 1); // Simple test to ensure the file is not empty
+}
