66e75f4ad0992131c81ba55a6febd7bc2cab907e
MaartyW@lap-pop <meweverling@gmail.com>
2025-05-06 23:59:49 +0200

    cleanup of test files

--- test/sha256_operations_test.cpp
+++ test/sha256_operations_test.cpp
@@ -2,9 +2,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-TEST(sha256_operations, rotl)
+TEST(sha256, operations)
 {
-	// Simple Test cases
+	// Simple Test cases for ROTL
 	EXPECT_EQ(ROTL(0x00000000, 1), 0x00000000);		// check if 0 stays 0
 	EXPECT_EQ(ROTL(0x00000001, 1), 0x00000002);		// standard check
 	EXPECT_EQ(ROTL(0x40000000, 1), 0x80000000);		// standard check higher value
@@ -16,11 +16,8 @@ TEST(sha256_operations, rotl)
 	EXPECT_EQ(ROTL(0xFFFFFFFF, 20), 0xFFFFFFFF);	// see if -1 stays -1
 
 	EXPECT_EQ(ROTL(0x00000001, 32), 0x00000001);	// overflow the rotation
-}
 
-TEST(sha256_operations, rotr)
-{
-	// Simple Test cases
+	// Simple Test cases for ROTR
 	EXPECT_EQ(ROTR(0x00000000, 1), 0x00000000);		// check if 0 stays 0
 	EXPECT_EQ(ROTR(0x00000002, 1), 0x00000001);		// standard check
 	EXPECT_EQ(ROTR(0x80000000, 1), 0x40000000);		// standard check higher value
@@ -34,9 +31,9 @@ TEST(sha256_operations, rotr)
 	EXPECT_EQ(ROTR(0x00000001, 32), 0x00000001);	// overflow the rotation
 }
 
-TEST(sha256_operations, ch)
+TEST(sha256, functions)
 {
-	// Simple Test cases
+	// Simple Test cases for Ch
 	EXPECT_EQ(Ch(0x00000000, 0x00000000, 0x00000000), 0x00000000);	// check if 0 stays 0
 	EXPECT_EQ(Ch(0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF), 0xFFFFFFFF);	// check if -1 stays -1
 	EXPECT_EQ(Ch(0xFFFFFFFF, 0x00000000, 0x00000000), 0x00000000);
